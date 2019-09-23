62a76846cfbfd7dad3c87140b1a6558451c35227
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-09-23 14:03:28 -0500

    added Login and PrivateRoute path for account after logging in to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Router, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className = "App">
+        <Switch>
+          <Route path = "/login" component = {Login} />
+          <PrivateRoute exact path = "/accounts" />
+        </Switch>
+
+      </div>
+
+
+    </Router>
+    
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -44,3 +44,5 @@ const Login = props => {
         </div>
     )
 }
+
+export default Login;
