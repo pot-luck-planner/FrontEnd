00867ac04e126f1e21c8e57170a356571b0c083f
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-09-23 17:13:29 -0400

    Data file added, if used

---
+++ src/components/Data.js
@@ -0,0 +1,5 @@
+//usable data for layout and such. Example, no endpoint was made for profile
+//image, but I want to layout with one. I could use this to hold that data. 
+//Maybe seperate generic gender pics that I can show with the seperate data 
+//returning?
+
