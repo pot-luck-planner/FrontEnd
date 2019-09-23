3c819f06059aaa29cdc380b57d16bf730cc779d0
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-09-23 13:57:45 -0500

    Created PriateRoute.js in utils

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -5,6 +5,7 @@ export const axiosWithAuth = () => {
     const token = localStorage.getItem('userToken');
 
     return axios.create ({
+        baseURL: 'https://potluck-planner-v2.herokuapp.com',
         header: {
             'Content-type': 'application/json',
             'Authorization': `${token}`,
